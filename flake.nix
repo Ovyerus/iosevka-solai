e4aa83fc2eeba7c80abd521cf3f1dbaa4693d06e
Ovyerus <ovy@ovyerus.com>
2023-11-22 12:06:04 +1100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages = {
+          normal = pkgs.iosevka.override {
+            set = "solai";
+            privateBuildPlan = import ./iosevka.nix {
+              family = "Iosevka Solai";
+              spacing = "normal";
+            };
+          };
+
+          term = pkgs.iosevka.override {
+            set = "solai-term";
+            privateBuildPlan = import ./iosevka.nix {
+              family = "Iosevka Solai Term";
+              spacing = "term";
+            };
+          };
+        };
+      }
+    );
+}
