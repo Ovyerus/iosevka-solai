9c3b5568a4e92a355d3c4acd47ba0c3c0e67e218
Ovyerus <ovy@ovyerus.com>
2023-11-23 00:19:18 +1100

    feat: add outputs for tarballs

--- flake.nix
+++ flake.nix
@@ -9,13 +9,31 @@
   outputs = {
     flake-utils,
     nixpkgs,
+    self,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+        selfPkgs = self.packages.${system};
+
+        mkTarball = output: src: let
+          inherit (src) version;
+          pname = "${src.pname}-tarball";
+        in
+          pkgs.runCommand "${pname}-${src.version}" {
+            inherit pname version src;
+            nativeBuildInputs = [pkgs.gnutar];
+          } ''
+            WORKDIR="$PWD"
+            cd $src
+            tar czf "$WORKDIR/${output}.tar.gz" ./*
+            mkdir -p $out/
+            cp -av "$WORKDIR/${output}.tar.gz" $out/
+          '';
       in {
         packages = {
+          # Font with ligatures and normal spacing (some characters are double width).
           normal = pkgs.iosevka.override {
             set = "solai";
             privateBuildPlan = import ./iosevka.nix {
@@ -24,12 +42,28 @@
             };
           };
 
+          # Font with ligatures and fixed spacing (all characters are single column).
           term = pkgs.iosevka.override {
             set = "solai-term";
             privateBuildPlan = import ./iosevka.nix {
               family = "Iosevka Solai Term";
               spacing = "term";
             };
+          };
+
+          all = pkgs.symlinkJoin {
+            name = "all";
+            paths = with selfPkgs; [normal term];
+          };
+
+          default = selfPkgs.all;
+
+          # Used for generating tarballs for GitHub releases.
+          tar-normal = mkTarball "iosevka-solai" selfPkgs.normal;
+          tar-term = mkTarball "iosevka-solai-term" selfPkgs.term;
+          tar = pkgs.symlinkJoin {
+            name = "tar";
+            paths = with selfPkgs; [tar-normal tar-term];
           };
         };
 
